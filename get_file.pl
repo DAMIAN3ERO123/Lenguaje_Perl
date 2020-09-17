be96318294134010c565831f04bd5e04494ba483
DAMIAN3ERO123 <36737516+DAMIAN3ERO123@users.noreply.github.com>
2020-09-17 16:19:43 -0500

    Add files via upload

--- get_file.pl
+++ get_file.pl
@@ -7,12 +7,42 @@ use LWP::Simple;
 
 #Script para obtener el archivo de una url y actualizar este archivo 
 #descargandolo cada 5mn aproximadamente
+#Saber si estan activos los sitios
+
 
 for (my $c = 1;$c < 2; $c--){ 
-	getstore("<https://pagina.com/archivo.txt>","paginas.txt") || die "No pude descargar el archivo";
-	sleep (300);
+	getstore("<https://openphish.com/feed.txt>","paginas.txt") || die "No pude descargar el archivo";
+	
 	print "\nSe actualizo el documento\n";
-	print scalar localtime(); 
+	print scalar localtime();
+	print "\n"; 
+
+	open (PAGINAS,'<','paginas.txt');
+	open (ACTIVOS,'+>>','sitios_activos.txt');
+
+	for my $elemento (<PAGINAS>){
+		chomp($elemento);
+		#print $elemento."\n";
+		my $pagina=`curl --silent -I $elemento`;
+		$pagina=`echo $pagina | head -n 1`; 
+		#print $pagina."\n";
+		if ($pagina =~ /200/ ){
+			print ACTIVOS "$elemento\n";
+			#print "Se agrego una dirección:\n";
+			#print $elemento."\n";
+			
+		}
+		next;
+	}
+
+
+
+	close (PAGINAS);
+	close (ACTIVOS);
+	print "Ya realice un análsis\n";
+	sleep (300);
+	system ('rm paginas.txt');
+	next;
 }
 
 
